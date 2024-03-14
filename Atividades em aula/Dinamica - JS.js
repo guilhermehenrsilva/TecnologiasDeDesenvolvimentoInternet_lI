fe8467f7d64c36b7e1b08ed571ca07f78c7420a3
Guilherme Henrique <guilherme_he_nrrique@hotmail.com>
2024-03-14 10:39:14 -0300

    Aula - 14/03/2024

--- Atividades em aula/Dinamica - JS.js
+++ Atividades em aula/Dinamica - JS.js
@@ -25,4 +25,27 @@ let myVar;
 let soma = myVar +1 ;
 console.log(soma);;
 
+
 //teste
+
+
+//Higher orden function 
+
+function maiorQue(){
+    let num = (Math.random() * 100).toFixed;
+    console.log(`Random ${num}`);
+
+    return function(valor){
+        return valor > num;
+    }
+}
+
+console.log(maiorQue()(50));
+
+
+// usandi uma variavel intermediaria 
+
+const funRef = maiorQue()
+console.log(funcRef(10));
+
+console.log(maiorQue);
---
+++ Atividades em aula/Teste_Funcao_14_03.js
@@ -0,0 +1,12 @@
+
+function maiorQue(){
+    let num = Math.random() * 100;
+    console.log(`Random ${num}`);
+
+    return maiorQue(50);
+    }
+
+
+console.log(maiorQue()(50));
+
+
