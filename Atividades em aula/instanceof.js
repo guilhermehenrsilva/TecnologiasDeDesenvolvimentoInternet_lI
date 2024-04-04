5fe84520ef76a909daf71fb045ae44c6635a576d
Guilherme Henrique <guilherme_he_nrrique@hotmail.com>
2024-04-04 10:49:09 -0300

    Atualizado - 04/04/2024

---
+++ Atividades em aula/instanceof.js
@@ -0,0 +1,7 @@
+let nativeStr = "Guilherme"
+let nativeStrFuntion = String("Henrique")
+let strAsAnObject = new String ("Henrique");
+
+console.log(typeof nativeStr); // String
+console.log(typeof nativeStrFuntion); //String
+console.log(typeof strAsAnObject);//object
---
+++ Atividades em aula/typeof.js
@@ -0,0 +1,20 @@
+let tipoDo10 = typeof 10;
+console,log(tipoDo10); // number
+console.log(typeof tipoDo10); //String
+
+let x = new String("A")
+console.log(typeof x);
+
+// Tipo de dados do JS --> null, 
+
+
+let und1 ;
+let und2 = undefined;
+let null1 = null;
+
+
+console.log(nula == indefinida);//true
+console.log(nula === indefinida);//False
+console.log(10 == "10");//true
+console.log(10 === "10");//False
+
